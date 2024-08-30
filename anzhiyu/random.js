60c49f4d4e79742aeaf34844d756b85bbc907b43
Echojjy <2589501880@qq.com>
2024-08-30 19:55:44 +0800

    Site updated: 2024-08-30 19:55:43

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/10/更新日志-1/","2024/08/29/hello-world/","2024/05/03/更新日志/"];function toRandomPost(){
+var posts=["2024/08/29/hello-world/","2024/05/10/更新日志-1/","2024/05/03/更新日志/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
